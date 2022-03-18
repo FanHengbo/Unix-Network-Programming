5f4773ca6683a5c76cc009e3f5d9061b5a4587a7
FanHengbo <fan_heng_bo@163.com>
2022-03-18 20:58:02 +0800

    sctp client

--- SCTP/client.c
+++ SCTP/client.c
@@ -1,6 +1,6 @@
 #include "unp.h"
-#include <linux/sctp.h>
-
+#include <netinet/sctp.h>
+void strcli(FILE* fp, int sockfd, struct sockaddr* psockaddr, size_t socklen);
 int main(int argc, char** argv)
 {
     struct sctp_event_subscribe evnts;
@@ -15,6 +15,7 @@ int main(int argc, char** argv)
     }
     if (argc > 2)
     {
+        printf("Send messages to all streams\n");
         one_to_all =1;
     }
     bzero(&servaddr,sizoof(servaddr));
@@ -22,8 +23,38 @@ int main(int argc, char** argv)
     servaddr.sin_port = htons(SERV_PORT);
     Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
     sockfd = socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
+  
+    bzero(&evnts, sizeof(evnts));
+    evnts.sctp_data_io_event = 1;
+    setsockopt(sockfd, IPPROTO_SCTP, SCTP_EVENTS, &evnts, sizeof(evnts));
 
-
+    strcli(stdin, sockfd, (SA *) &servaddr, sizeof(servaddr));
     return 0;
 
+}
+void strcli(FILE* fp, int sockfd, struct sockaddr* psockaddr, size_t socklen)
+{
+    struct sockaddr_in peeraddr;
+    struct sctp_sndrcvinfo sri;
+    char sendline[MAXLINE];
+    char receivline[MAXLINE];
+    int read_size;
+    int msg_flags;
+
+    bzero(&sri, sizoof(sri));
+    while (fgets(sendline, MAXLINE, fp))
+    {
+        if (sendline[0] != '[')
+        {
+            printf("Please input again, [stream] text\n");
+            continue;
+        }
+        sri.sinfo_stream = strtol(&sendline[1], NULL, 0);
+        sctp_sendmsg(sockfd, sendline, sizeof(sendline), psockaddr, socklen, 0, 0, sri.sinfo_stream, 0, 0);
+
+        read_size = sctp_recvmsg(sockfd, receivline, sizeof(receivline), (SA *) &peeraddr, sizeof(peeraddr), &sri, &msg_flags);
+        printf("from str :%d seq :%d (assoc:0x%x)\n", sri.sinfo_stream, sri.sinfo_ssn, (u_int) sri.sinfo_assoc_id);
+        writen(stdout, receivline, sizeof(receivline));
+
+    }
 }
