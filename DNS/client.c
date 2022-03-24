6da7ac0c5bc1f2f6351a5d6de062ee6d4ee342da
FanHengbo <fan_heng_bo@163.com>
2022-03-24 20:41:00 +0800

    gethostnyname

--- DNS/client.c
+++ DNS/client.c
@@ -5,23 +5,33 @@ int main(int argc, char** argv)
     char *ptr;
     char **pptr;
     struct hostent *hptr;
+    char buf[INET_ADDRSTRLEN];
 
     while(--argc > 0)
     {
         ptr = *++argv;
-        if (hptr = gethostbyname(ptr) == NULL)
+        if ((hptr = gethostbyname(ptr)) == NULL)
         {
             printf("gethostbyname error\n");
-            exit(0);
+            continue;
         }
-    }
-    printf("official hostname : %s\n", hptr->h_name);
-    for (pptr = hptr->h_aliases; *pptr != NULL; pptr++)
-    {
-        printf("aliases: %s\n", *pptr);
-    }
     
-
+        printf("official hostname : %s\n", hptr->h_name);
+        for (pptr = hptr->h_aliases; *pptr != NULL; pptr++)
+        {
+            printf("aliases: %s\n", *pptr);
+        }
+        switch(hptr->h_addrtype)
+        {
+            case AF_INET:
+            for (pptr =hptr->h_addr_list; *pptr; pptr++)
+            {
+                printf("addresss : %s\n", inet_ntop(AF_INET, *pptr, buf, sizeof(buf)));
+            }
+            break;
+        }
+    
+    }
     return 0;
 
 }
