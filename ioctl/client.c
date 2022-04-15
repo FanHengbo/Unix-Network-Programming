de66b700e207989700c474d668722ce35dcf1cb8
FanHengbo <fan_heng_bo@163.com>
2022-04-15 19:59:46 +0800

    gei_ifi_info

--- ioctl/client.c
+++ ioctl/client.c
@@ -1,4 +1,6 @@
 #include "unpifi.h"
+#include "utils.h"
+
 int main(int argc, char **argv)
 {
     struct ifi_info *ifi, *ifihead;
@@ -6,6 +8,8 @@ int main(int argc, char **argv)
     struct sockaddr_in client;
     int family;
     int alias;
+    int index;
+    int len;
 
     if (argc != 3)
     {
@@ -30,8 +34,48 @@ int main(int argc, char **argv)
     {
 
         printf("%s\n", ifi->ifi_name);
-        
+        if ((index = ifi->ifi_index) != 0)
+        {
+            printf("Index %d", index);
+        }
+        printf("<");
+        if (ifi->ifi_flags & IFF_UP)
+            printf("UP ");
+        if (ifi->ifi_flags & IFF_BROADCAST)
+            printf("BROADCAST ");
+        if (ifi->ifi_flags & IFF_MULTICAST)
+            printf("MULTICAST ");
+        if (ifi->ifi_flags & IFF_LOOPBACK)
+            printf("LO ");
+        if (ifi->ifi_flags & IFF_POINTOPOINT)
+            printf("P2P");
+        printf(">\n");
+        if ((len = ifi->ifi_hlen) > 0)
+        {
+            for (int i = 0; i < len; i++)
+            {
+                printf("%s%x", (i == len-1) ? " " : ":", ifi->ifi_haddr[i]);
+            }
+            printf("\n");
+        }
+        if (ifi->ifi_mtu)
+        {
+            printf("MTU : %d\n", ifi->ifi_mtu);
+        }
+        if ((sa = ifi->ifi_addr))
+        {
+            printf("IP address is %s\n", sock_ntop(sa, sizeof(*sa)));
+        }
+        if ((sa = ifi->ifi_brdaddr))
+        {
+            printf("Broadcast address is %s\n", sock_ntop(sa, sizeof(*sa)));
+        }
+        if ((sa = ifi->ifi_dstaddr))
+        {
+            printf("Destination address is %s\n", sock_ntop(sa, sizeof(*sa)));
+        }
     }
+    free_ifi_info(ifihead);
 
     return 0;
 }
